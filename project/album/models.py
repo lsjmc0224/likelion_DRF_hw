b856d89b76bb30fc659b04cbd403a0898c46e0a9
lsjmc0224 <lsjmc0224@dgu.edu>
2023-07-04 09:44:07 +0900

    DRF 과제 중

---
+++ project/album/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    year = models.PositiveIntegerField()
+
+class Track(models.Model):
+    id = models.AutoField(primary_key=True)
+    album = models.ForeignKey(Album, blank=False, null=False, on_delete=models.CASCADE, related_name='tracks')
+    number = models.IntegerField()
+    title = models.CharField(max_length=50)
---
+++ project/album/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import *
+
+class AlbumSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    tracks = serializers.SerializerMethodField(read_only=True)
+
+    def get_tracks(self, instance):
+        serializer = TrackSerializer(instance.tracks, many=True)
+        return serializer.data
+
+    class Meta:
+        model = Album
+        fields = ['id', 'title', 'artist', 'description', 'year', 'tracks']
+
+class TrackSerializer(serializers.ModelSerializer):
+
+    album = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Track
+        fields = '__all__'
+        read_only_fields = ['album']
+
+    def get_album(self, instance):
+        serializer = AlbumSerializer(instance.album, many=True)
+        return serializer.data
+
+# class TrackSerializer(serializers.ModelSerializer):
+    
+#     class Meta:
+#         model = Track
+#         fields = '__all__'
+#         read_only_fields = ['album']
